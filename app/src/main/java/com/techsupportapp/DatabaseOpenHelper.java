f8660352ea6d8fae63af7a66b8feab0b70bd0930
Anton <maxkost4@gmail.com>
2016-09-02 23:15:56 +0300

    Added permissions to read/write data for sdcard. Started working with SQLite databases.  (Monarch)

--- app/src/main/java/com/techsupportapp/DatabaseOpenHelper.java
+++ app/src/main/java/com/techsupportapp/DatabaseOpenHelper.java
@@ -4,15 +4,36 @@ import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 import android.provider.ContactsContract;
+import android.util.Log;
 
-public class DatabaseOpenHelper extends SQLiteOpenHelper{
+public class DatabaseOpenHelper extends SQLiteOpenHelper implements BaseColumns{
 
     //region Fields
+
+    private static final String PERSONALITY_TABLE = "personality_table";
+
+    private static final String LOGIN_COLUMN = "login";
+    private static final String PASSWORD_COLUMN = "password";
+
+    private static final String USER_TABLE = "user_table";
+
+    private static final String SURNAME_COLUMN = "surname";
+    private static final String NAME_COLUMN = "name";
+    private static final String IS_ADMIN_COLUMN = "is_admin";
+    private static final String COMPANY_ID_COLUMN = "company_id";
 
     //endregion
 
     //region Scripts
+
+    private static final String DATABASE_CREATE_USER_TABLE = "create table " + USER_TABLE + " ("
+            + BaseColumns._ID + " integer primary key autoincrement, "
+            + SURNAME_COLUMN + " text not null, "
+            + NAME_COLUMN + " text not null, "
+            + IS_ADMIN_COLUMN + " text not null, "
+            + COMPANY_ID_COLUMN + " integer not null);";
 
     //endregion
 
@@ -30,11 +51,16 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(DATABASE_CREATE_USER_TABLE); //TODO исправить после организации скачивания базы данных с сервера
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w("SQLite", "Обновляемся с версии " + oldVersion + " на версию " + newVersion);
 
+        db.execSQL("DROP TABLE IF IT EXISTS " + USER_TABLE);
+
+        onCreate(db);
     }
+
 }
--- app/src/main/java/com/techsupportapp/SignInActivity.java
+++ app/src/main/java/com/techsupportapp/SignInActivity.java
@@ -1,12 +1,18 @@
 package com.techsupportapp;
 
 import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -46,11 +52,35 @@ public class SignInActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        signInBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readPasswordDatabase(); // Временная реализация
+                //TODO Написать логику входа в приложения через логин/пароль
+            }
+        });
     }
 
     private void loadPasswordDatabase()
     {
         //TODO Скачивание базы данных с паролями с сервера.
+    }
+
+    private void readPasswordDatabase()
+    {
+        File file = new File("/sdcard/Android/data/com.techsuppotapp/passwordDatabase.db");
+        File directory = new File("/sdcard/Android/data/com.techsuppotapp/");
+
+        if (!file.exists())
+        {
+            if (!directory.exists())
+                directory.mkdirs();
+            //TODO используем метод loadPasswordDatabase
+        }
+
+        DatabaseOpenHelper helper = new DatabaseOpenHelper(this, file.getPath(), 1); //Временное решение до реализации скачивания базы с сервера
+        SQLiteDatabase base = helper.getReadableDatabase();
     }
 
     @Override
