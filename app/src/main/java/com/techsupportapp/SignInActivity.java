9304ffd94cf0845c0e0a1d33cf46e957b7ba649d
Serg <spg4526@gmail.com>
2016-09-05 17:04:58 +0300

    add method savePassAndLogin

--- app/src/main/java/com/techsupportapp/SignInActivity.java
+++ app/src/main/java/com/techsupportapp/SignInActivity.java
@@ -41,7 +41,6 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
         initializeComponents();
 
         setEvents();
